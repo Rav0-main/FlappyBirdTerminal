3e54b131c4890dbb919093a573c4f9f92e9891d7
Rav0 <139210981+Rav0-main@users.noreply.github.com>
2026-08-13 21:01:16 +0300

    Revert "Added best score and fps counters. (#4)"
    This reverts commit b224ce33d1f9df07976205545ab31df04eb062a4.

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,5 +1,4 @@
 #include "timer.h"
-#include <thread>
 
 double Timer::GetFrameTime()
 {
@@ -7,16 +6,5 @@ double Timer::GetFrameTime()
     std::chrono::duration<double> elapsed = current_time - prev_time_;
     prev_time_ = current_time;
 
-    prev_frame_time_ = elapsed.count();
-    return prev_frame_time_;
-}
-
-unsigned short Timer::GetFPS() const noexcept
-{
-    return 1 / prev_frame_time_;
-}
-
-void Timer::Tick() const noexcept
-{
-    std::this_thread::sleep_for(std::chrono::microseconds(tick_time_));
+    return elapsed.count();
 }
